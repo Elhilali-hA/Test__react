1e13100338cb2489332ba6bfc0ead11874473cb5
Elhilali-hA <Elhilali-hA>
2022-05-18 17:44:27 +0100

    test react

---
+++ Test-react/my-test/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+import Navbar from './components/Navbar';
+import First from './components/First_section';
+import Second from './components/Second_section';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <First />
+      <Second />
+      
+      
+
+    </div>
+  );
+}
+
+export default App;
